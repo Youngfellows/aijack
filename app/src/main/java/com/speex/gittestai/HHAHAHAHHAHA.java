ef703882d5865ff1cdb8fc761785dcfab3b1f3ec
Your Name <you@example.com>
2018-06-15 01:00:51 +0800

    嘻嘻嘻嘻

---
+++ app/src/main/java/com/speex/gittestai/HHAHAHAHHAHA.java
@@ -0,0 +1,15 @@
+package com.speex.gittestai;
+
+import android.util.Log;
+
+/**
+ * Created by Byron on 2018/6/15.
+ */
+
+public class HHAHAHAHHAHA {
+    private String TAG = this.getClass().getSimpleName();
+    public void test()
+    {
+        Log.i(TAG, "test: xxxxxxxxxxxxxxxsafdsasadfsdaf");
+    }
+}
