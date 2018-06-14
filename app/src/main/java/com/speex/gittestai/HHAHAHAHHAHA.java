147d3f768e3e6f603d13ece335a070cbdb20a121
Your Name <you@example.com>
2018-06-15 01:34:22 +0800

    是否打算打的说法

--- app/src/main/java/com/speex/gittestai/HHAHAHAHHAHA.java
+++ app/src/main/java/com/speex/gittestai/HHAHAHAHHAHA.java
@@ -10,20 +10,12 @@ public class HHAHAHAHHAHA {
     private String TAG = this.getClass().getSimpleName();
     public void test()
     {
-
-
         for (int i = 0; i < 100; i++) {
-            Log.d(TAG, "test: xxxxxxxxxxxxxxxxxxxx");
-            ////深圳市南山区软件长翼棘豆
-            ////深圳市南山区软件长翼棘豆
             ////深圳市南山区软件长翼棘豆
             Log.d(TAG, "test: sssssssssssssssssssssss");
-            ////深圳市南山区软件长翼棘豆
             ////深圳市南山区软件长翼棘豆
             Log.d(TAG, "test: sssssssssssssssssssssss");
         }
-
-        
         Log.i(TAG, "test: xxxxxxxxxxxxxxxsafdsasadfsdaf");
     }
 }
